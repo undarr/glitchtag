d5a19e29dfae847bd0b42e6fc2896ed18cbfd1c3
Ulfred Chan <101036368+undarr@users.noreply.github.com>
2025-05-28 14:38:03 +0800

    update glitch

--- app.py
+++ app.py
@@ -1,70 +1,66 @@
 import streamlit as st
+import streamlit.components.v1 as components
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 import time
-import json
-import re
-
-# ------------- Settings for Pages -----------
+import threading
+import sys
+from frontend import fendfile
+from datetime import datetime
+import queue
 st.set_page_config(layout="wide")
 
-# Keep text only
-def get_clues():
-    driver = None
-    try:
-        # Using on Local
-        options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
-        options.add_argument('--disable-gpu')
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
-                                  options=options)
-        driver.get("https://www.minutecryptic.com")
-        button = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.bg-mc-pink'))
-        )
-        # Click the button
-        button.click()
-        # Retrieve local storage data
-        puzzle_button = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-sentry-component="PuzzlePiece"]'))
-        )
-        # Click the second button
-        puzzle_button.click()
-        puzzle_button.send_keys('a')
-        local_storage = driver.execute_script("return window.localStorage;")
-        mcp=json.loads(local_storage['mc-puzzle'])
-        a=mcp['answer'].lower()
-        q=' '.join([i['text'] for i in mcp['clue']])+' ('+','.join([str(len(j)) for j in a.split()])+')'
-        h1=mcp['hints'][0]['text']
-        h2=mcp['hints'][1]['text']
-        h3=mcp['hints'][2]['text']
-        ht1=mcp['hints'][0]['type']
-        ht2=mcp['hints'][1]['type']
-        ht3=mcp['hints'][2]['type']
-        v=mcp['explainerVideo']
-        sn=mcp['setterName']
-        driver.get("https://dailycrypticle.com/dailyclue.html")
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
-        dc=['','','','']
-        while '' in dc:
-            dc[0]=driver.execute_script("return targetWord;")
-            dc[1]=driver.execute_script("return clueData;")
-            dc[2]=driver.execute_script("return urlData;")
-            dc[3]=driver.execute_script("return definitionData;")
-        dc[1]+=" ("+str(len(dc[0]))+")"
-        driver.quit()
-        return (' ()minc() '.join([q,a,h1,h2,h3,ht1,ht2,ht3,v,sn])+' ()big() '+' ()dc() '.join(dc))
-    except Exception as e:
-        st.write(f"DEBUG:INIT_DRIVER:ERROR:{e}")
-    finally:
-        if driver is not None: driver.quit()
-    return None
+if 'codeshare' not in st.session_state:
+    st.session_state.codeshare = "123"
 
-# ---------------- Page & UI/UX Components ------------------------
-if __name__ == "__main__":
-    d=get_clues()
-    st.write(d)
+if 'thread_queue' not in st.session_state:
+    st.session_state.thread_queue = queue.Queue()
+
+def my_threaded_function(q: queue.Queue):
+    print("Thread: Started")
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")  # Optional: run headless
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://codeshare.io/ueue")
+    code_mirror_editor_div = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.CodeMirror")))
+    cur = ""
+    print("Driverloaded")
+    while True:
+        initial_code = driver.execute_script("return arguments[0].CodeMirror.getValue();", code_mirror_editor_div)
+        if (initial_code!=cur):
+            cur=initial_code
+            q.put({"status": "running", "message": cur})
+            print("Added new")
+        #time.sleep(0.1)
+    q.put({"status": "progress", "message": "56"})
+    time.sleep(3) # Simulate more work
+    q.put({"status": "completed", "message": "67"})
+    print("Thread: Finished and put final message in queue")
+
+if 'codeshareget' not in st.session_state:
+    st.session_state.codeshareget = False
+    thread = threading.Thread(target=my_threaded_function, args=(st.session_state.thread_queue,))
+    thread.start()
+    st.success("Thread started. Updates should appear live.")
+
+def pq():
+    try:
+        while not st.session_state.thread_queue.empty():
+            message_from_thread = st.session_state.thread_queue.get_nowait()
+            print(f"Main: Received from queue: {message_from_thread}")
+            st.session_state.codeshare = message_from_thread["message"]
+    except queue.Empty:
+        pass # Queue is empty, nothing to process
+
+def toXY(stri):
+    return({"x": str(int(stri)//15+1),"y": str(int(stri)%15+1)})
+pq()
+props = {'runner': toXY(st.session_state.codeshare)}
+value = fendfile(**props,key="123")
+st.header('Streamlit')
+st.write('Received from component: ', value)
